dd6f6da3e211ec8b40af09a57d8eea59403983e4
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2022-10-18 22:27:53 -0300

    #1-release: ajustando formatação e pequenos bugs

--- utils/img_util.py
+++ utils/img_util.py
@@ -1,17 +1,19 @@
 import cv2
+import os
+
 
 class Img_util(object):
-	def config_img(src):
-		img = cv2.imread(os.path.abspath('.') + src)
-		rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-		ipt = self.config_input(rgb)
-		return img,rgb,ipt
+    def config_img(config_input, src):
+        img = cv2.imread(os.path.abspath(".") + src)
+        rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        ipt = config_input(rgb)
+        return img, rgb, ipt
 
-	def bounding_box(inp, img, i, cor=(0, 255, 0), tam_fonte=2):
-		x = inp['left'][i]
-		y = inp['top'][i]
-		w = inp['width'][i]
-		h = inp['height'][i]
+    def bounding_box(inp, img, i, cor=(0, 255, 0), tam_fonte=2):
+        x = inp["left"][i]
+        y = inp["top"][i]
+        w = inp["width"][i]
+        h = inp["height"][i]
 
-		cv2.rectangle(img, (x, y), (x + w, y + h), cor, tam_fonte)
-		return x, y, img
+        cv2.rectangle(img, (x, y), (x + w, y + h), cor, tam_fonte)
+        return x, y, img
